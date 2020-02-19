e3a6f9d02cba3cc4bf84a8e595647194d90675ee
Sajeev Saluja <sajeevsa@usc.edu>
2020-02-18 19:59:22 -0800

    replacing kvstore.h

---
+++ kvstore/kvstore.h
@@ -0,0 +1,10 @@
+#include <unordered_map>
+
+class KeyValueStore {
+ public:
+  int put(const std::string&, const std::string&);
+  int get(const std::string&);
+  int remove(const std::string&);
+ private:
+  std::unordered_map<std::string, std::string> map_;
+};
